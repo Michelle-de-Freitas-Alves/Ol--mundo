ee68837caefcaa06aea5a6eee60b0aac45cf273b
Michelle de Freitas <michelle.freitas89@gmail.com>
2023-03-04 17:58:18 -0300

    commit initial

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TiposVariaveis {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Olá meu nome é michelle");
+		
+		sc.close();
+
+	}
+
+}
